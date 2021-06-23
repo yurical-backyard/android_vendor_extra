00c0270ba34b9e4dd5fd07459a29c7eac0d94bfe
Danny Lin <danny@kdrag0n.dev>
2021-06-23 01:22:07 -0700

    extra: vendorsetup: Override hostname to avoid leaking info
    [yurical: Changed proton-build for android-build]
    Signed-off-by: Yurical <yurical1@outlook.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,6 @@
 
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
+
+# Override hostname to make builds more reproducible and avoid leaking info
+export BUILD_HOSTNAME=android-build
