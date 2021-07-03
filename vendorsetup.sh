437405983a5dd2d4e6f7c265478516abdf35b0ec
Danny Lin <danny@kdrag0n.dev>
2021-07-03 02:51:41 -0700

    extra: vendorsetup: Override more host metadata

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
 
-# Override hostname to make builds more reproducible and avoid leaking info
+# Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_HOSTNAME=android-build
+export BUILD_USERNAME=android-build
