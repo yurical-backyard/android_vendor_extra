50069d5d2ef88c89b7b2f0f749ac131bc3867f82
Yurical <yurical1@outlook.com>
2021-07-11 23:44:46 +0900

    extra: Add package remover

---
+++ package_remover/Android.mk
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := package_remover
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+LOCAL_OVERRIDES_PACKAGES += \
+
+include $(BUILD_PREBUILT)
--- product.mk
+++ product.mk
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
 
+# Build package remover
+PRODUCT_PACKAGES += package_remover
+
 # Inherit main Makefile
 $(call inherit-product, vendor/extra/configs/main.mk)
 
