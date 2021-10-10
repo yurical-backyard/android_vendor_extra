2a5d66cb40c0815162dba80064cb753929865694
Danny Lin <danny@kdrag0n.dev>
2021-10-10 04:52:20 -0700

    extra: vendorsetup: Update host metadata
    Matches the Android 12 build sandbox: https://android.googlesource.com/platform/build/+/refs/tags/android-12.0.0_r2/Changes.md

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,5 +4,5 @@
 # SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
 
 # Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
-export BUILD_USERNAME=android-build
