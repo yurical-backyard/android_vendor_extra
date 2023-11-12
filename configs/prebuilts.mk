768060162ef56a29a50d1775bb3649fac4b96d1f
Yurical <yurical1@outlook.com>
2023-11-12 17:35:30 +0900

    extra: prebuilts: Import Auxio v3.4.3
    Add Auxio developed by OxygenCobalt as a music player replacing others.
    The source code is public on GitHub[1], and the apk file is derived
    from GitHub release.
    
    [1]: https://github.com/OxygenCobalt/Auxio
    
    Signed-off-by: Yurical <yurical1@outlook.com>

--- configs/prebuilts.mk
+++ configs/prebuilts.mk
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
 
+# Auxio
+PRODUCT_PACKAGES += \
+    Auxio
+
 # Material Files
 PRODUCT_PACKAGES += \
     MaterialFiles
