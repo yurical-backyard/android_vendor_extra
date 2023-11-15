4d6751b775c84711b7e07347916f4ca8a22aaf1f
Yurical <yurical1@outlook.com>
2023-11-15 23:00:18 +0900

    extra: Checkout font importing scripts from ProtonAOSP
    Checkout the font importing scripts from ProtonAOSP/android_vendor_proton@fced239
    and hook up configs/fonts.mk, which is a stripped down version of
    fonts/font.mk.

--- product.mk
+++ product.mk
@@ -6,3 +6,6 @@ $(call inherit-product, vendor/extra/configs/main.mk)
 
 # Inherit prebuilts Makefile
 $(call inherit-product, vendor/extra/configs/prebuilts.mk)
+
+# Inherit font Makefile
+$(call inherit-product, vendor/extra/configs/fonts.mk)
