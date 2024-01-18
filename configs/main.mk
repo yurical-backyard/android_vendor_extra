f26ae49e3d267ca795b1adec61af9eba00e73920
Yurical <yurical1g@gmail.com>
2024-01-18 17:45:31 +0900

    extra: Inherit empty main Makefile

--- configs/main.mk
+++ configs/main.mk
@@ -1,5 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright 2024 Yurical <yurical1g@gmail.com>
-
-# Inherit main Makefile
-$(call inherit-product, vendor/extra/configs/main.mk)
